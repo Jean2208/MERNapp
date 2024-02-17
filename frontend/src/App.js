c95700d8272cd6f0328c09722ab342587f489f95
Jean Penso <jeanpaulpenso01@live.com>
2024-02-17 15:33:24 -0600

    Added route for Workout page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Workout from './pages/Workout';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/workout/:id' element={<Workout />}/>
+            <Route path='*' element={<NotFound />}/>
           </Routes>
         </div>
       </BrowserRouter>

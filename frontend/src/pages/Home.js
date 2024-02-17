f06b33cf285bb787c62a6904779bb77aecc64cec
Jean Penso <jeanpaulpenso01@live.com>
2024-02-16 19:58:53 -0600

    Added context for local workout state

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,31 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect} from 'react';
 import WorkoutDetails from '../components/WorkoutDetails';
 import WorkoutForm from '../components/WorkoutForm';
+import { WorkoutContext } from '../context/WorkoutContext';
+
 
 const Home = () => {
 
-    const [workouts, setWorkouts] = useState(null)
+    const {workouts, dispatch} = useContext(WorkoutContext)
 
     // useEffect to load fetch once
     useEffect(() => {
+
         const fetchWorkouts = async () => {
             const response = await fetch('/workouts')
             const json = await response.json()
 
             if (response.ok) {
-                setWorkouts(json)
+                dispatch({type: 'SET_WORKOUTS', payload: json})
             }
         }
         fetchWorkouts()
-    }, [])
+    }, [dispatch])
+
     return (
         <div className="home">
             <div className="workouts">
                 {workouts && workouts.map((workout) => (
-                    <WorkoutDetails key={workout.id} workout={workout} />
+                    <WorkoutDetails key={workout._id} workout={workout} />
                 ))}
             </div>
-            <WorkoutForm/>
+            <WorkoutForm />
         </div>
 
     );

3d37f03f5200cb632648bc5391b100a6c5ae992c
Jean Penso <jeanpaulpenso01@live.com>
2024-02-16 20:01:26 -0600

    Added conext to delete workout from local state

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,10 +1,29 @@
+import { useContext } from "react"
+import { WorkoutContext } from "../context/WorkoutContext"
+
 const WorkoutDetails = ({ workout }) => {
+
+    const {dispatch} = useContext(WorkoutContext)
+
+    const handleDelete = async () => {
+        const response = await fetch('/workouts/' + workout._id, {
+            method: 'DELETE'
+        })
+
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+    }
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p>{workout.createdAt}</p>
+            <button onClick={handleDelete}>delete</button>
         </div>
     );
 }
